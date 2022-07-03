a253dcd227838ecf38a5cbee0765829f3d4c4355
jpcouhe <98466099+jpcouhe@users.noreply.github.com>
2022-07-03 17:06:37 +0200

    packages npm cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const dotenv = require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
-const db = require("./config/db-config");
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
@@ -13,8 +13,6 @@ if (process.env.NODE_ENV === "development") {
     app.use(morgan("combined"));
 }
 
-// Paramètre de la Base de donnée
-
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 const postRoutes = require("./routes/post.routes");
@@ -24,28 +22,7 @@ const commentRoutes = require("./routes/comment.routes");
 
 /* A middleware that parses the body of the request and makes it available on the `req.body` property. */
 app.use(express.json());
-
-/* Connecting to the database. */
-db.connect((err) => {
-    if (err) {
-        res.status(500).json({ error: "Impossible de se connecter à la base de donnée" });
-    }
-    console.log("Database connected !!");
-});
-
-/* A middleware that allows the server to accept requests from other domains. */
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    next();
-});
-
-/* Serving the images folder as a static folder. */
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(cors());
 
 app.use("/api/like", likeRoutes);
 app.use("/api/auth", authRoutes);
