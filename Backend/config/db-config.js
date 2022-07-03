f54cd3fb4d81cacbd0061ed69875e05d897e37fc
jpcouhe <98466099+jpcouhe@users.noreply.github.com>
2022-07-03 17:06:26 +0200

    Config avec pool

--- Backend/config/db-config.js
+++ Backend/config/db-config.js
@@ -1,12 +1,14 @@
 const sql = require("mysql2");
 
-const db = sql.createConnection({
+const db = sql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
     database: process.env.DATABASE_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
-
 
 module.exports = db;
